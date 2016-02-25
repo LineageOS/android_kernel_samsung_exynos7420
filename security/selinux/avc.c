2a2b7b7aeaa29b5a8ece2e29d993ed7585c17025
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-02-25 10:59:23 -0500

    Fixup 565877b61a from LL to MM
    Change-Id: I324189356242419833e6028501ddaa8310c22793

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -997,8 +997,8 @@ static noinline int avc_denied(u32 ssid, u32 tsid,
 				u16 cmd, unsigned flags,
 				struct av_decision *avd)
 {
-	avc_update_node(AVC_CALLBACK_GRANT, requested, ssid,
-				tsid, tclass, avd->seqno);
+	avc_update_node(AVC_CALLBACK_GRANT, requested, cmd, ssid,
+				tsid, tclass, avd->seqno, NULL, flags);
 	return 0;
 }
 
